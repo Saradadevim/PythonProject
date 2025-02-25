4fcc72a4a6e61b1deb67add994b35c0b9cf28a9f
Thomas-1301 <thomasvettiyil365@gmail.com>
2025-02-25 13:03:57 +0530

    Added another print_message function

--- model/Technician.py
+++ model/Technician.py
@@ -1,6 +1,9 @@
 class TechnicianPage():
     def print_message(self):
         print("This is a message from the TechnicianPage class.")
+    def print_another_message(self):
+        print("This is another message from the TechnicianPage class.")
 
 tech_page = TechnicianPage()
 tech_page.print_message()
+tech_page.print_another_message()
