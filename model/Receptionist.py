69cc360eb7cbe02661a8f9e435d6584e98c1756f
Stains <stainsdas007@gmail.com>
2025-02-25 14:16:44 +0530

    updated receptionist

--- model/Receptionist.py
+++ model/Receptionist.py
@@ -1,2 +1,8 @@
-class receptionist:
-    pass
+class Receptionist:
+    def __init__(self, name):
+        self.name = name  # Example attribute
+
+    def print_message(self):
+        print(f"Hello, welcome to the clinic! My name is {self.name}. How can I assist you?")
+RecepPage=Receptionist()
+RecepPage.print_message()
